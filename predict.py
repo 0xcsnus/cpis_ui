6a726fafddfe067ff4d0f052f01b65f18785cdf0
Domun Shaarvin <domunshaarvin@pop-os.localdomain>
2022-07-31 19:20:05 +0400

    mod

--- predict.py
+++ predict.py
@@ -4,13 +4,16 @@ def model(event, context):
  try:
     data = json.loads(event.get('body'))
     data = data['text']
-    newModel = tf.keras.models.load_model('./final_model')
+    newModel = tf.keras.models.load_model('./myModel',compile=False)
     predict = newModel.predict([data])
-    data = float(predict[0][0][0])
+    data = float(predict[0][0])
+    pred = "Suicidal" if data>=0 else "Not suicidal"
     body = {
-        "prediction": data
+        "prediction": pred
     }
     return {"statusCode":200, "body":json.dumps(body)}
  except:
      return {"statusCode":500}
+
+
 
